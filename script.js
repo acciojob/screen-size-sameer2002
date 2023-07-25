90016c3ca91797c14c5dbd90c35b1d7982f3f910
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-25 14:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function resize() {
+	const width = window.innerWidth;
+    const heigth = window.innerHeight;
+
+    const div = document.getElementById("sizeInfo");
+
+    div.innerHTML = `<h1>Width: ${width} and Height: ${heigth}`
+}
+ window.addEventListener("resize",resize);
+
+ resize();
